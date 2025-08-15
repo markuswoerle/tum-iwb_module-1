462dbb2e9ab228972213e3f76d0955a32c311d45
Markus Woerle <markus.woerle@iwb.tum.de>
2025-08-15 11:01:53 +0200

    Add files via upload

--- MATLAB CODE/main.m
+++ MATLAB CODE/main.m
@@ -1,1 +1,12 @@
+% Technical University of Munich (TUM)
+% TUM School of Engineering and Design
+% Institute for Machine Tools and Industrial Management (iwb)
+% Markus Woerle, M.Sc.
 
+% Closing all figures, clearing all variables, and clearing the command window
+close all;
+clear variables;
+clc;
+
+% Calling the function to analyze the susceptibility for hydrogen-induced material damage
+susceptibility();
